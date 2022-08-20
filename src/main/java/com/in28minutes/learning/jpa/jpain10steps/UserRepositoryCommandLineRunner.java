e93f186e1d0a1a80f7a5bfcfc2a6a6739ffb695d
unknown <genzr05@gmail.com>
2022-08-20 10:50:28 +1000

    Implemented a User Entity DAO service as well as a Repository pattern for comparison

---
+++ src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -0,0 +1,26 @@
+package com.in28minutes.learning.jpa.jpain10steps;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+import com.in28minutes.learning.jpa.jpain10steps.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepositoryCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(UserRepositoryCommandLineRunner.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User("Jill", "Admin");
+        userRepository.save(user);
+        log.info("New user is created: " + user);
+    }
+
+}
