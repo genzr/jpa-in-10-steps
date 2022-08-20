116b982507ce9995463cb751f747b640dd26b0ce
unknown <genzr05@gmail.com>
2022-08-20 10:55:03 +1000

    added some additional Spring Data JPA methods

--- src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -19,8 +22,16 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         User user = new User("Jill", "Admin");
+
         userRepository.save(user);
         log.info("New user is created: " + user);
+
+        Optional<User> userWithIdOne = userRepository.findById(1L);
+        log.info("User is retrieved: " + userWithIdOne);
+
+        List<User> users = userRepository.findAll();
+        log.info("Users retrieved: " + users);
+
     }
 
 }
